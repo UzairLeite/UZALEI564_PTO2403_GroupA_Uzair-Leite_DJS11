6f9c90cb572f6bd1c134e6fa6a4cdc8fe420813d
UzairLeite <102726763+UzairLeite@users.noreply.github.com>
2025-02-24 17:25:28 +0200

    Added util files for local storage

---
+++ src/utils/api.ts
@@ -0,0 +1,16 @@
+import { API_BASE_URL } from '../constants';
+
+export const fetchShows = async () => {
+  const response = await fetch(`${API_BASE_URL}`);
+  return response.json();
+};
+
+export const fetchShowById = async (id: string) => {
+  const response = await fetch(`${API_BASE_URL}/id/${id}`);
+  return response.json();
+};
+
+export const fetchGenreById = async (id: number) => {
+  const response = await fetch(`${API_BASE_URL}/genre/${id}`);
+  return response.json();
+};
---
+++ src/utils/storage.ts
@@ -0,0 +1,19 @@
+import localforage from 'localforage';
+
+export const getFavorites = async (): Promise<string[]> => {
+  return (await localforage.getItem<string[]>('favorites')) || [];
+};
+
+export const addFavorite = async (episodeId: string) => {
+  const favorites = await getFavorites();
+  if (!favorites.includes(episodeId)) {
+    favorites.push(episodeId);
+    await localforage.setItem('favorites', favorites);
+  }
+};
+
+export const removeFavorite = async (episodeId: string) => {
+  const favorites = await getFavorites();
+  const updatedFavorites = favorites.filter((id) => id !== episodeId);
+  await localforage.setItem('favorites', updatedFavorites);
+};
